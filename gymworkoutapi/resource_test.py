bb0ac19cced9f85786e3648760ec1ababc47c14e
ksaily <katri.saily@elisanet.fi>
2023-03-05 21:48:09 +0200

    added post, put and delete tests

--- gymworkoutapi/resource_test.py
+++ gymworkoutapi/resource_test.py
@@ -71,6 +71,27 @@ def _populate_db():
     
     db.session.commit()
 
+def _get_user_json(number=1):
+    """
+    Creates a valid user JSON object to be used for PUT and POST tests.
+    """
+    return {"username": "extra_user{}".format(number), 
+        "height": 150.0, "weight": 50.0}
+
+def _get_workout_json(number=1):
+    """
+    Creates a valid workout JSON object to be used for PUT and POST tests.
+    """
+    return {"user_id":1,"workout_name": "extra_workout{}".format(number),
+        "favorite":True}
+
+def _get_movement_json(number=1):
+    """
+    Creates a valid movement JSON object to be used for PUT and POST tests.
+    """
+    return {"workout_id":1,"movement_name": "extra_movement{}".format(number), 
+        "sets": 3, "reps": 5}
+
 
 class TestUserCollection(object):
     """
@@ -82,63 +103,292 @@ class TestUserCollection(object):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
         body = json.loads(resp.data)
-        assert len(body) == 3
+        assert len(body[0]) == 5
         for item in body:
             assert "username" in item
             assert "height" in item
             assert "weight" in item
 
     def test_post(self, client):
-        pass
+        """
+        Tests POST method by checking the following:
+        error codes, valid request receives a 201 response
+        """
+        valid = _get_user_json()
 
+        #test with wrong content type
+        resp = client.post(self.RESOURCE_URL, data=json.dumps(valid))
+        assert resp.status_code == 415
+
+        #test with valid and see that it exists afterward
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 201
+        assert resp.headers.endswith(self.RESOURCE_URL + valid["username"] + "/")
+        resp = client.get(resp.headers)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["username"] == "extra_user1"
+        assert body["height"] == 150.0
+        assert body["weight"] == 50.0
+
+        #send same data again for 409
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+
+        #remove height field for 400
+        valid.pop("weight")
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
 
 
 class TestUserItem(object):
     """
     This class implements tests for each HTTP method in TestUserItem resource. 
     """
+    RESOURCE_URL = "/api/users/test_user1/"
+    INVALID_URL = "/api/users/non_user1/"
+    MODIFIED_URL = "/api/users/extra_user1/"
+
     def test_get(self, client):
-        pass
+        """
+        Tests the GET method. Checks the following:
+        response status code is 200, expected attributes are present,
+        all of the items in DB population are present.
+        """
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["username"] == "test_user1"
+        #assert body["height"] == float
+        #assert body["weight"] == float
+        resp = client.get(self.INVALID_URL)
+        assert resp.status_code == 404
     
     def test_put(self, client):
-        pass
+        """
+        Tests the PUT method. Checks the following:
+        error codes, valid request receives a 201 response,
+        when name is changed the sensor can't be found from its new URI
+        """
+        valid = _get_user_json()
+
+        #test with wrong content type
+        resp = client.put(self.RESOURCE_URL, data=json.dumps(valid))
+        assert resp.status_code == 415
+        resp = client.put(self.INVALID_URL, json=valid)
+        assert resp.status_code == 404
         
-    def delete(self, client):
-        pass
+        #test with another user's name
+        valid["username"] = "test_user2"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+
+        #test with valid (change user)
+        valid["username"] = "test_user1"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 201
+
+        #remove field for 400
+        valid.pop("height")
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
+
+        valid = _get_user_json()
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        resp = client.get(self.MODIFIED_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["height"] == valid["height"]
+        
+    def test_delete(self, client):
+        """
+        Tests the DELETE method. Checks the following:
+        valid request receives 201 response and trying to GET
+        the user afterwards results in 404 and trying to delete a user
+        that doesn't exist results in 404.
+        """
+        resp = client.delete(self.RESOURCE_URL)
+        assert resp.status_code == 201
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 404
+        resp = client.delete(self.INVALID_URL)
+        assert resp.status_code == 404
         
 class TestWorkoutCollection(object):
     """
     This class implements tests for each HTTP method in WorkoutCollection resource. 
     """
+    RESOURCE_URL = "/api/users/test_user1/workouts/"
+
     def test_get(self, client):
-        pass
-    
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert len(body[0]) == 3
+        for item in body:
+            assert "workout_name" in item
+            assert "favorite" in item
+            assert "user_id" in item
+
     def test_post(self, client):
-        pass
+        """
+        Tests POST method by checking the following:
+        error codes, valid request receives a 201 response
+        """
+        valid = _get_movement_json()
+
+        #test with wrong content type
+        resp = client.post(self.RESOURCE_URL, data=json.dumps(valid))
+        assert resp.status_code == 400
+
+        #test with valid and see that it exists afterward
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 201
+        body = json.loads(resp.data)
+        assert body["workout_name"] == "extra_workout1"
+        assert body["favorite"] == True
+
+        #send same data again for 409
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+
+        #remove favorite field for 400
+        valid.pop("favorite")
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
         
 class TestWorkoutItem(object):
     """
     This class implements tests for each HTTP method in TestWorkoutItem resource. 
     """
+    RESOURCE_URL = "/api/users/test_user1/workouts/test_workout1/"
+    INVALID_URL = "/api/users/test_user1/workouts/non_workout1/"
+    MODIFIED_URL = "/api/users/test_user1/workouts/extra_workout1/"
+
     def test_get(self, client):
-        pass
+        """
+        Tests the GET method. Checks the following:
+        response status code is 200, expected attributes are present,
+        all of the items in DB population are present.
+        """
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["workout_name"] == "test_workout1"
+        #assert body["favorite"] == True
+        resp = client.get(self.INVALID_URL)
+        assert resp.status_code == 404
     
     def test_put(self, client):
-        pass
-    
-    def test_post(self, client):
-        pass
+        """
+        Tests the PUT method. Checks the following:
+        error codes, valid request receives a 201 response,
+        when name is changed the sensor can't be found from its new URI
+        """
+        valid = _get_workout_json()
+
+        #test with wrong content type
+        resp = client.put(self.RESOURCE_URL, data=json.dumps(valid))
+        assert resp.status_code == 415
+        resp = client.put(self.INVALID_URL, json=valid)
+        assert resp.status_code == 404
+        
+        #test with another workout's name
+        valid["workout_name"] = "test_workout2"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+
+        #test with valid (change workout)
+        valid["workout_name"] = "test_workout1"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 201
+
+        #remove field for 400
+        valid.pop("favorite")
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
+
+        valid = _get_workout_json()
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        resp = client.get(self.MODIFIED_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["workout_name"] == valid["workout_name"]
         
     def test_delete(self, client):
-        pass
+        """
+        Tests the DELETE method. Checks the following:
+        valid request receives 201 response and trying to GET
+        the user afterwards results in 404 and trying to delete a user
+        that doesn't exist results in 404.
+        """
+        resp = client.delete(self.RESOURCE_URL)
+        assert resp.status_code == 201
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 404
+        resp = client.delete(self.INVALID_URL)
+        assert resp.status_code == 404
+    
+    def test_post(self, client):
+        
+        valid = _get_movement_json()
+        
+        #test with wrong content type
+        resp = client.post(self.RESOURCE_URL, data=json.dumps(valid))
+        print(resp)
+        assert resp.status_code == 415
+
+        #test with valid and see that it exists afterward
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 201
+        body = json.loads(resp.data)
+        assert body["workout_name"] == "extra_workout1"
+        assert body["favorite"] == True
+
+        # send same data again for 409
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+        
+        # remove field for 400
+        valid.pop("favorite")
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
+        
         
 class TestMovementItem(object):
     """
     This class implements tests for each HTTP method in TestMovementItem resource. 
     """
+    RESOURCE_URL = "/api/users/test_user1/workouts/test_workout1/test_movement1/"
+    INVALID_URL = "/api/users/test_user1/workouts/test_workout1/non_movement1/"
+    MODIFIED_URL = "/api/users/test_user1/workouts/test_workout1/extra_movement1/"
+
     def test_get(self, client):
-        pass
+        """
+        Tests the GET method. Checks the following:
+        response status code is 200, expected attributes are present,
+        all of the items in DB population are present.
+        """
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["movement_name"] == "test_movement1"
+        #assert body["sets"]
+        #assert body["reps"]
+        resp = client.get(self.INVALID_URL)
+        assert resp.status_code == 404
     
     def test_delete(self, client):
-        pass
+        """
+        Tests the DELETE method. Checks the following:
+        valid request receives 201 response and trying to GET
+        the user afterwards results in 404 and trying to delete a user
+        that doesn't exist results in 404.
+        """
+        resp = client.delete(self.RESOURCE_URL)
+        assert resp.status_code == 201
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 404
+        resp = client.delete(self.INVALID_URL)
+        assert resp.status_code == 404
         
